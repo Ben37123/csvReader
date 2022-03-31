dee8a30901ce48c7fe25eb0291af361f548ba39d
nathanfry <npfry7@gmail.com>
2022-03-31 08:23:45 -0500

    new sub branch test 1.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("nathanfryy test sub branch 1.1");
+	System.out.print("nathanfryy test sub branch 1.2");
     }
 }
