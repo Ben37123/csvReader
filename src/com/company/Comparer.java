6a197431d68ec59feb284eb9f3816eb00eda24d0
Bean <ben.blankenbiller@gmail.com>
2022-05-11 23:04:51 -0500

    final commit

--- src/com/company/Comparer.java
+++ src/com/company/Comparer.java
@@ -1,55 +1,88 @@
 package com.company;
 import java.util.*;
 
+import static java.lang.Math.abs;
+
 public class Comparer{
 
     public List find_similarities(List<List<String>> a, List<List<String>> b, int numValues) {
+
+
         List<Pair<Integer, Double>> pairList = new ArrayList<>();
         List<List<String>> finalList = new ArrayList<>();
         List<List<String>> combinedList = a;
+
         for (int i = 0; i < b.size();i++) {
-            if (i == 0) {
-            }
+            if (i == 0);
             else {
                 combinedList.add(b.get(i));
             }
         }
 
-        for (int r = 0; r < numValues; r++) {
-            //String valueString = new String();
-            //for (int i = 0; i < numValues; i++) {
-            //    valueString += i + ": " + combinedList.get(0).get(i) + "\n";
-            //}
-            //Scanner scan = new Scanner(System.in);
-            //System.out.println("How would like to match the files?\n"+"Enter the corresponding number\n"+ valueString);
-            //String answer = scan.nextLine();
-            //int intAnswer = Integer.parseInt(answer);
-            int intAnswer = r;
-            for (int i = 0; i < combinedList.size() - 1; i++) {
+        boolean isTrue = true;
+        int counter = 0;
+        String answer = "";
+        while (isTrue) {
+            if (counter > 0) {
+                System.out.print("Please enter 1 or 2 !!!!!\n");
+            }
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Would you like to match the files according all values or a single value? Enter 1 or 2 \n1: All Values\n2: Single Value");
+            answer = scan.nextLine();
+            if (Integer.valueOf(answer) == 1) {
+                isTrue = false;
+            }
+            if (Integer.valueOf(answer) == 2) {
+                isTrue = false;
+            }
+            counter++;
+        }
+
+        if (Integer.valueOf(answer) == 1) {
+            for (int i = 0; i < combinedList.size(); i++) {
+                double charValue = 0;
                 if (i == 0) ;
                 else {
-                    for (int u = intAnswer; u < combinedList.get(i).size(); u = u + numValues) {
-                        List<String> characterList = new ArrayList<>();
-                        int charValue = 0;
-                        for (int e = 0; e < combinedList.get(i).get(u).length(); e++) {
-                            String value = combinedList.get(i).get(u);
-                            characterList.add(String.valueOf(value.charAt(e)));
-                        }
-                        for (int k = 0; k < characterList.size(); k++) {
-                            String currentChar = characterList.get(k);
-                            for (int j = 0; j < combinedList.size(); j++) {
-                                if (j == 0) ;
-                                if (j == i) ;
+                    for (int u = 0; u < combinedList.get(i).size(); u++) {
+                        //if the value is an integer it does this for loop
+                        if (isInteger(combinedList.get(i).get(u))) {
+                            int intValue = Integer.parseInt(combinedList.get(i).get(u));
+                            for (int p = 0; p < combinedList.size(); p++) {
+                                if (p == 0) ;
                                 else {
-                                    for (int m = intAnswer; m < combinedList.get(j).size(); m = m + numValues) {
-                                        List<String> tempCharList = new ArrayList<>();
-                                        for (int n = 0; n < combinedList.get(j).get(m).length(); n++) {
-                                            String tempValue = combinedList.get(j).get(m);
-                                            tempCharList.add(String.valueOf(tempValue.charAt(n)));
-                                            for (int y = 0; y < tempCharList.size(); y++) {
-                                                String compareChar = tempCharList.get(y);
-                                                if (currentChar.equalsIgnoreCase(compareChar)) {
-                                                    charValue++;
+                                    for (int h = u; h < combinedList.get(p).size(); h = h + numValues) {
+                                        int tempIntValue = Integer.parseInt(combinedList.get(p).get(h));
+                                        charValue = charValue + (abs(intValue - tempIntValue)) / intValue;
+                                    }
+                                }
+                            }
+                        }
+                        //if the value is a string it does this for loop
+                        else {
+                            List<String> characterList = new ArrayList<>();
+                            for (int e = 0; e < combinedList.get(i).get(u).length(); e++) {
+                                String value = combinedList.get(i).get(u);
+                                characterList.add(String.valueOf(value.charAt(e)));
+                            }
+                            for (int k = 0; k < characterList.size(); k++) {
+                                String currentChar = characterList.get(k);
+                                for (int j = 0; j < combinedList.size(); j++) {
+                                    if (j == 0) ;
+                                    if (j == i) ;
+                                    else {
+                                        for (int m = u; m < combinedList.get(j).size(); m = m + numValues) {
+                                            List<String> tempCharList = new ArrayList<>();
+                                            for (int n = 0; n < combinedList.get(j).get(m).length(); n++) {
+                                                String tempValue = combinedList.get(j).get(m);
+                                                tempCharList.add(String.valueOf(tempValue.charAt(n)));
+                                                for (int y = 0; y < tempCharList.size(); y++) {
+                                                    String compareChar = tempCharList.get(y);
+                                                    if (currentChar == " ") ;
+                                                    else {
+                                                        if (currentChar.equalsIgnoreCase(compareChar)) {
+                                                            charValue++;
+                                                        }
+                                                    }
                                                 }
                                             }
                                         }
@@ -57,24 +90,119 @@ public class Comparer{
                                 }
                             }
                         }
-                        double fuzzyLogicValue = (double) charValue / combinedList.get(i).get(u).length();
-                        pairList.add(new Pair<>(i, fuzzyLogicValue));
                     }
+                }
+                double fuzzyLogicValue = 0;
+                for (int z = 0; z < combinedList.get(i).size(); z++) {
+                    fuzzyLogicValue = fuzzyLogicValue + charValue / combinedList.get(i).get(z).length();
+                }
+                if (i == 0) ;
+                else {
+                    pairList.add(new Pair<>(i, fuzzyLogicValue));
                 }
             }
         }
+
+
+        if (Integer.valueOf(answer) == 2) {
+            String valueString = new String();
+            for (int i = 0; i < numValues; i++) {
+                valueString += i + ": " + combinedList.get(0).get(i) + "\n";
+            }
+
+
+            boolean checkTrue = true;
+            int numCounter = 0;
+            String newAnswer = "";
+            while (checkTrue) {
+                if (numCounter > 0) {
+                    System.out.print("Please enter 0 to "+(numValues-1)+"!!!!!\n");
+                }
+                Scanner scan = new Scanner(System.in);
+                System.out.println("How would like to match the files?\n" + "Enter the corresponding number\n" + valueString);
+                newAnswer = scan.nextLine();
+                for (int number = 0; number < numValues; number++)
+                if (Integer.valueOf(newAnswer) == number) {
+                    checkTrue = false;
+                }
+                numCounter++;
+            }
+
+
+            int intAnswer = Integer.parseInt(newAnswer);
+            for (int i = 0; i < combinedList.size(); i++) {
+                double charValue = 0;
+                if (i == 0) ;
+                else {
+                    for (int u = intAnswer; u < combinedList.get(i).size(); u = u + numValues) {
+                        if (isInteger(combinedList.get(i).get(u))) {
+                            int intValue = Integer.parseInt(combinedList.get(i).get(u));
+                            for (int p = 0; p < combinedList.size(); p++) {
+                                if (p == 0) ;
+                                else {
+                                    for (int h = u; h < combinedList.get(p).size(); h = h + numValues) {
+                                        int tempIntValue = Integer.parseInt(combinedList.get(p).get(h));
+                                        charValue = charValue + (abs(intValue - tempIntValue)) / intValue;
+                                    }
+                                }
+                            }
+                        }
+                        else {
+                            List<String> characterList = new ArrayList<>();
+                            for (int e = 0; e < combinedList.get(i).get(u).length(); e++) {
+                                String value = combinedList.get(i).get(u);
+                                characterList.add(String.valueOf(value.charAt(e)));
+                            }
+                            for (int k = 0; k < characterList.size(); k++) {
+                                String currentChar = characterList.get(k);
+                                for (int j = 0; j < combinedList.size(); j++) {
+                                    if (j == 0) ;
+                                    if (j == i) ;
+                                    else {
+                                        for (int m = u; m < combinedList.get(j).size(); m = m + numValues) {
+                                            List<String> tempCharList = new ArrayList<>();
+                                            for (int n = 0; n < combinedList.get(j).get(m).length(); n++) {
+                                                String tempValue = combinedList.get(j).get(m);
+                                                tempCharList.add(String.valueOf(tempValue.charAt(n)));
+                                                for (int y = 0; y < tempCharList.size(); y++) {
+                                                    String compareChar = tempCharList.get(y);
+                                                    if (currentChar == " ") ;
+                                                    else {
+                                                        if (currentChar.equalsIgnoreCase(compareChar)) {
+                                                            charValue++;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                // creates the values for which the fuzzy logic is used
+                double fuzzyLogicValue = 0;
+                for (int z = 0; z < combinedList.get(i).size(); z++) {
+                    fuzzyLogicValue = fuzzyLogicValue + charValue / combinedList.get(i).get(z).length();
+                }
+                if (i == 0) ;
+                else {
+                    pairList.add(new Pair<>(i, fuzzyLogicValue));
+                }
+            }
+        }
+
+        //sorts the list of pairs based off the fuzzyLogicValue from greatest to least
         Collections.sort(pairList, Comparator.comparing(p -> -p.second));
         for (int q = 0; q < pairList.size(); q++) {
             finalList.add(combinedList.get(pairList.get(q).first));
-        }
-        for (int poop = 0; poop < finalList.size(); poop++) {
-            System.out.print(finalList.get(poop) + "\n");
         }
         return finalList;
     }
 
 
-
+    //built in java Pair method not available on inteliJ so I pulled this from internet; creates a pair of two values
     class Pair<U, V>
     {
         public final U first;       // the first field of a pair
@@ -129,6 +257,16 @@ public class Comparer{
         }
     }
 
+    //also got this isInteger from internet; checks if the given string is an integer or not
+    public boolean isInteger (String input) { //Pass in string
+        try { //Try to make the input into an integer
+            Integer.parseInt( input );
+            return true; //Return true if it works
+        }
+        catch( Exception e ) {
+            return false; //If it doesn't work return false
+        }
+    }
 
 
 }
--- src/com/company/Creator.java
+++ src/com/company/Creator.java
@@ -1,7 +1,27 @@
 package com.company;
 
-public class Creator {
-    public static void create_new_csv() {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Creator {
+    public static void create_new_csv(List<List<String>> finalList) throws IOException {
+
+        //initializes the fileWriter that will write the lists of strings onto the new csv file
+        FileWriter writer = new FileWriter("src/com/company/CSVfiles/createdCSV.csv");
+
+        //for loop that goes through each line and each string in each line and adds them to the new file
+        for (int i = 0; i < finalList.size(); i++) {
+            List<String> test = new ArrayList<>();
+            for (int e = 0; e < finalList.get(i).size(); e++) {
+                test.add(finalList.get(i).get(e));
+            }
+            test.add("\n");
+            String collect = test.stream().collect(Collectors.joining(","));
+            writer.write(collect);
+        }
+        writer.close();
     }
 }
